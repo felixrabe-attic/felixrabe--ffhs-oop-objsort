13bd4c45e9d40faa9ee2628ed755d06a6b0ae7e6
Felix Rabe <public@felixrabe.net>
2011-11-28 17:44:06 +0100

    More example data

--- StudentExampleData.java
+++ StudentExampleData.java
@@ -11,7 +11,15 @@ public class StudentExampleData {
     public static Student[] build() {
         return new Student[] {
             new Student.Builder("4")
-                .first("Archimedes").last("Syrakus").age(2296).gpa(6.0).build()
+                .first("Archimedes").last("Syrakus").age(2296).gpa(6.0).build(),
+            new Student.Builder("3")
+                .first("Fritzli").last("Schneider").age(9).gpa(4).build(),
+            new Student.Builder("1")
+                .first("Walterli").last("Tell").age(12).gpa(4).build(),
+            new Student.Builder("5")
+                .first("Greengrey").last("vonRotz").age(20).gpa(4).build(),
+            new Student.Builder("2")
+                .first("Maria").last("Stuart").age(467).gpa(4).build()
         };
     }
 }
