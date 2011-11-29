5475748cb03f171a957ce790d7c1742bad6f26f8
Felix Rabe <public@felixrabe.net>
2011-11-29 01:24:49 +0100

    Big reduction

--- Main.java
+++ Main.java
@@ -9,20 +9,24 @@
 public class Main {
     /** Main program. */
     public static void main(String[] args) {
-        Sort sort;
-        Student[] students;
-        
-        System.out.println("### SORTING USING BUBBLESORT ###");
-        sortUsing(StudentExampleData.build(), new BubbleSort<Student>());
-    }
-    
-    /** Sort students using the provided algorithm. */
-    private static void sortUsing(Student[] students, Sort<Student> sort) {
+        Student[] students = StudentExampleData.build();
         print("Unsorted", students);
-        print("Sorted by number",
-            sort.cmp(new StudentNumberComparator()).sort(students));
-        print("Sorted by last name",
-            sort.cmp(new StudentLastNameComparator()).sort(students));
+
+        sortInPlaceUsing(students, new java.util.Comparator<Student>() {
+            /** Sort students by number. */
+            public int compare(Student a, Student b) {
+                return a.getNumber() - b.getNumber();
+            }
+        });
+        print("Sorted by number", students);
+
+        sortInPlaceUsing(students, new java.util.Comparator<Student>() {
+            /** Sort students by last name. */
+            public int compare(Student a, Student b) {
+                return a.getLastName().compareTo(b.getLastName());
+            }
+        });
+        print("Sorted by last name", students);
     }
     
     private static void print(String caption, Student[] students) {
@@ -31,5 +35,38 @@ public class Main {
             System.out.println(s);
         }
         System.out.println();
+    }
+    
+    /** Sort the specified array in-place.
+     *
+     * Uses the bubble sort algorithm and the specified
+     * {@link java.util.Comparator} object.
+     *
+     * @param array the array to sort
+     */
+    private static void sortInPlaceUsing(Student[] array,
+                                         java.util.Comparator<Student> comparator) {
+        boolean hadToSwap;
+        do {
+            hadToSwap = false;
+            for (int first = 0; first < array.length - 1; first++) {
+                int second = first + 1;
+                if (comparator.compare(array[first], array[second]) > 0) {
+                    swap(array, first, second);
+                    hadToSwap = true;
+                }
+            }
+        } while(hadToSwap);
+    }
+
+    /** Swap two elements in the given array.
+     * @param array the array in which to swap the elements
+     * @param first the index of the first element to swap
+     * @param second the index of the second element to swap
+     */
+    private static void swap(Student[] array, int first, int second) {
+        Student tmp = array[first];
+        array[first] = array[second];
+        array[second] = tmp;
     }
 }
